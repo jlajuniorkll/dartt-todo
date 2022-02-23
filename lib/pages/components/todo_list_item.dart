374f68d520ac3ce1b1c49c9ae42abf73f6b8a815
MacBook Junior <jlajuniorkll@icloud.com.br>
2022-02-23 17:06:45 -0300

    funcs_principais

---
+++ lib/pages/components/todo_list_item.dart
@@ -0,0 +1,57 @@
+import 'package:dartt_todo/pages/models/todo.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({
+    Key? key,
+    required this.todo,
+    required this.onDelete,
+  }) : super(key: key);
+
+  final Todo todo;
+  final Function(Todo) onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2.0),
+      child: Slidable(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: const Color.fromRGBO(238, 238, 238, 1),
+          ),
+          padding: const EdgeInsets.all(16),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Text(
+              DateFormat("dd/MM/yyyy - HH:mm").format(todo.dateTime),
+              style: const TextStyle(fontSize: 12),
+            ),
+            Text(
+              todo.title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ]),
+        ),
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              flex: 4,
+              onPressed: (context) {
+                onDelete(todo);
+              },
+              backgroundColor: const Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,39 +1,103 @@
+import 'package:dartt_todo/pages/components/todo_list_item.dart';
+import 'package:dartt_todo/pages/models/todo.dart';
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
 
   @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todoController = TextEditingController();
+
+  List<Todo> tarefas = [];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-        ),
-        child: Row(
-          children: [
-            const Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Adicione uma tarefa",
-                  hintText: "Ex: Ir ao mercado",
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todoController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Ex: Ir ao mercado",
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      String text = todoController.text;
+                      setState(() {
+                        Todo newTodo =
+                            Todo(title: text, dateTime: DateTime.now());
+                        tarefas.add(newTodo);
+                      });
+                      todoController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        padding: const EdgeInsets.all(14.0)),
+                    child: const Icon(Icons.add, size: 30),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (Todo tarefa in tarefas)
+                      TodoListItem(todo: tarefa, onDelete: onDelete),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.blue, padding: const EdgeInsets.all(14.0)),
-              child: const Icon(Icons.add, size: 30),
-            ),
-          ],
+              const SizedBox(
+                height: 16.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                          "Você possui ${tarefas.length} tarefas pendentes")),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        padding: const EdgeInsets.all(14.0)),
+                    child: const Text("Limpar Tudo"),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    ));
+      )),
+    );
+  }
+
+  void onDelete(Todo tarefa) {
+    setState(() {
+      tarefas.remove(tarefa);
+    });
   }
 }
